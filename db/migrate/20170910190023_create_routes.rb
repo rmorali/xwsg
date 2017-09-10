1d9869385a97c56819285a49e78484ef19294b10
rmorali <rodrigo.morali@gmail.com>
2017-09-10 13:59:23 -0700

    created route table

---
+++ db/migrate/20170910190023_create_routes.rb
@@ -0,0 +1,11 @@
+class CreateRoutes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :routes do |t|
+      t.integer :vector_a
+      t.integer :vector_b
+      t.integer :distance
+
+      t.timestamps
+    end
+  end
+end
