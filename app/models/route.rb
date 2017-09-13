da4ff59454f1ce7efbd0b5417f46d62d184dfeda
rmorali <rodrigo.morali@gmail.com>
2017-09-13 07:50:27 -0700

    returned routing methods to its model

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,5 +2,33 @@ class Route < ApplicationRecord
 
   belongs_to :vector_a, class_name: 'Planet', foreign_key: 'vector_a'
   belongs_to :vector_b, class_name: 'Planet', foreign_key: 'vector_b'
-  
+
+  def self.edges
+    edges = []
+    Route.all.each do |r|
+      edges << [r.vector_a, r.vector_b, r.distance]
+      edges << [r.vector_b, r.vector_a, r.distance]
+    end
+    edges
+  end
+
+  def self.path(origin, destination)
+    Dijkstra.new(origin, destination, self.edges).shortest_path
+  end
+
+  def self.cost(origin, destination)
+    Dijkstra.new(origin, destination, self.edges).cost
+  end
+
+  def self.in_range(origin, hyperdrive)
+    edges = Route.edges
+    destinations = []
+    Planet.all.each do |planet|
+      path = Dijkstra.new(origin, planet, edges).shortest_path
+      path.each do |p|
+        destinations << p if Dijkstra.new(origin, p, edges).cost <= hyperdrive
+      end
+    end
+    destinations.uniq
+  end
 end
