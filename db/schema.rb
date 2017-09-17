e0d9d7e9f75a2415ef68dfa128a268a5f9f73c62
rmorali <rodrigo.morali@gmail.com>
2017-09-17 10:53:09 -0700

    created user relation to squads

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170917131159) do
+ActiveRecord::Schema.define(version: 20170917175132) do
 
   create_table "factions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 20170917131159) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "squad_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
