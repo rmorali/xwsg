f4679d31bd3b4a1f2bfce809bd9528b07d31d9ac
rmorali <rodrigo.morali@gmail.com>
2017-09-17 06:22:04 -0700

    started to configure devise

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  before_filter :authenticate_user!, :except => [:index]
+
+  def index
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914212430) do
+ActiveRecord::Schema.define(version: 20170917131159) do
 
   create_table "factions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 20170914212430) do
     t.datetime "updated_at", null: false
     t.integer "round_id"
     t.integer "carrier_id"
+    t.integer "destination_id"
+    t.integer "arrives_in"
+    t.integer "ready_in"
     t.index ["planet_id"], name: "index_fleets_on_planet_id"
     t.index ["squad_id"], name: "index_fleets_on_squad_id"
     t.index ["unit_id"], name: "index_fleets_on_unit_id"
@@ -83,11 +86,28 @@ ActiveRecord::Schema.define(version: 20170914212430) do
     t.integer "metals"
     t.integer "rare_elements"
     t.integer "producing_time"
-    t.integer "load_weigth"
-    t.integer "load_capacity"
+    t.integer "weight"
+    t.integer "capacity"
     t.boolean "groupable"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "fleets", "planets"
