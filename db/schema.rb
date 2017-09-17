789449037e2e2a5fa5480c8f135d26e87ca5d5ff
rmorali <rodrigo.morali@gmail.com>
2017-09-16 17:22:21 -0700

    changed weight and capacity columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916003404) do
+ActiveRecord::Schema.define(version: 20170917000439) do
 
   create_table "factions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20170916003404) do
     t.integer "round_id"
     t.integer "carrier_id"
     t.integer "destination_id"
-    t.integer "arrive_in"
+    t.integer "arrives_in"
     t.index ["planet_id"], name: "index_fleets_on_planet_id"
     t.index ["squad_id"], name: "index_fleets_on_squad_id"
     t.index ["unit_id"], name: "index_fleets_on_unit_id"
@@ -85,8 +85,8 @@ ActiveRecord::Schema.define(version: 20170916003404) do
     t.integer "metals"
     t.integer "rare_elements"
     t.integer "producing_time"
-    t.integer "load_weigth"
-    t.integer "load_capacity"
+    t.integer "weight"
+    t.integer "capacity"
     t.boolean "groupable"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
