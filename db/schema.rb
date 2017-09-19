652756b8c7e68a81ce50391b665c3751f002235f
rmorali <rodrigo.morali@gmail.com>
2017-09-18 17:46:27 -0700

    reloaded schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,108 +10,109 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_917_191_318) do
-  create_table 'factions', force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema.define(version: 20170917191318) do
+
+  create_table "factions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'fleets', force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
-    t.integer 'quantity'
-    t.bigint 'unit_id'
-    t.bigint 'squad_id'
-    t.bigint 'planet_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'round_id'
-    t.integer 'carrier_id'
-    t.integer 'destination_id'
-    t.integer 'arrives_in'
-    t.integer 'ready_in'
-    t.index ['planet_id'], name: 'index_fleets_on_planet_id'
-    t.index ['squad_id'], name: 'index_fleets_on_squad_id'
-    t.index ['unit_id'], name: 'index_fleets_on_unit_id'
+  create_table "fleets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "quantity"
+    t.bigint "unit_id"
+    t.bigint "squad_id"
+    t.bigint "planet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "round_id"
+    t.integer "carrier_id"
+    t.integer "destination_id"
+    t.integer "arrives_in"
+    t.integer "ready_in"
+    t.index ["planet_id"], name: "index_fleets_on_planet_id"
+    t.index ["squad_id"], name: "index_fleets_on_squad_id"
+    t.index ["unit_id"], name: "index_fleets_on_unit_id"
   end
 
-  create_table 'planets', force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
-    t.string 'name'
-    t.string 'sector'
-    t.bigint 'population'
-    t.integer 'credits'
-    t.integer 'metals'
-    t.integer 'rare_elements'
-    t.string 'domination'
-    t.integer 'x'
-    t.integer 'y'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "planets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.string "sector"
+    t.bigint "population"
+    t.integer "credits"
+    t.integer "metals"
+    t.integer "rare_elements"
+    t.string "domination"
+    t.integer "x"
+    t.integer "y"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'rounds', force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
-    t.integer 'phase', default: 0
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "rounds", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "phase", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'routes', force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
-    t.integer 'vector_a'
-    t.integer 'vector_b'
-    t.integer 'distance'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "routes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "vector_a"
+    t.integer "vector_b"
+    t.integer "distance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'squads', force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
-    t.string 'name'
-    t.integer 'credits'
-    t.integer 'metals'
-    t.integer 'rare_elements'
-    t.string 'url'
-    t.boolean 'ready'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'faction_id'
-    t.string 'color'
+  create_table "squads", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.integer "credits"
+    t.integer "metals"
+    t.integer "rare_elements"
+    t.string "url"
+    t.boolean "ready"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "faction_id"
+    t.string "color"
   end
 
-  create_table 'units', force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
-    t.string 'name'
-    t.string 'acronym'
-    t.string 'type'
-    t.string 'terrain'
-    t.integer 'faction_mask'
-    t.integer 'hyperdrive'
-    t.integer 'credits'
-    t.integer 'metals'
-    t.integer 'rare_elements'
-    t.integer 'producing_time'
-    t.integer 'weight'
-    t.integer 'capacity'
-    t.boolean 'groupable'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "units", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.string "acronym"
+    t.string "type"
+    t.string "terrain"
+    t.integer "faction_mask"
+    t.integer "hyperdrive"
+    t.integer "credits"
+    t.integer "metals"
+    t.integer "rare_elements"
+    t.integer "producing_time"
+    t.integer "weight"
+    t.integer "capacity"
+    t.boolean "groupable"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer 'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string 'current_sign_in_ip'
-    t.string 'last_sign_in_ip'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'squad_id'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "squad_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'fleets', 'planets'
-  add_foreign_key 'fleets', 'squads'
-  add_foreign_key 'fleets', 'units'
+  add_foreign_key "fleets", "planets"
+  add_foreign_key "fleets", "squads"
+  add_foreign_key "fleets", "units"
 end
