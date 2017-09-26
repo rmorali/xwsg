035799f4b8a2efd6abc27ccfd79645259cabd7b8
rmorali <rodrigo.morali@gmail.com>
2017-09-26 04:47:20 -0700

    implementing embark

--- app/controllers/fleets_controller.rb
+++ app/controllers/fleets_controller.rb
@@ -3,13 +3,14 @@ class FleetsController < ApplicationController
   def edit
     @fleet = Fleet.find(params[:id])
     @destinations = Route.in_range_for(@fleet)
+    @embarkables = @fleet.embarkables
     respond_to do |format|
       format.html
       format.js
     end
   end
 
-  def update
+  def move
     @fleet = Fleet.find(params[:id])
     @destination = Planet.find_by(id: fleet_params[:destination].to_i)
     @quantity = fleet_params[:quantity].to_i
@@ -17,10 +18,19 @@ class FleetsController < ApplicationController
     redirect_to squads_map_path
   end
 
+  def embark
+    @fleet = Fleet.find(params[:id])
+    raise embark_params.inspect
+  end
+
   private
 
   def fleet_params
     params.require(:fleet).permit(:destination, :quantity)
+  end
+
+  def embark_params
+    params.require(:custom).permit(:quantity)
   end
 
 end
