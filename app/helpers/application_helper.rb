e0942b66d1fccc237d5e4f50c40590b371780125
rmorali <rodrigo.morali@gmail.com>
2017-10-05 08:42:26 -0300

    shows carried fleets aside

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,2 @@
 module ApplicationHelper
-  def adjust_x(sector)
-    case sector
-    when 1, 4, 7
-      return 0
-    when 2, 5, 8
-      return 33
-    when 3, 6, 9
-      return 66
-    end
-  end
-
-  def adjust_y(sector)
-    case sector
-    when 1, 2, 3
-      return 0
-    when 4, 5, 6
-      return 33
-    when 7, 8, 9
-      return 66
-    end
-  end
 end
---
+++ app/helpers/squad_helper.rb
@@ -0,0 +1,33 @@
+module SquadHelper
+	  def adjust_x(sector)
+    case sector
+    when 1, 4, 7
+      return 0
+    when 2, 5, 8
+      return 33
+    when 3, 6, 9
+      return 66
+    end
+  end
+
+  def adjust_y(sector)
+    case sector
+    when 1, 2, 3
+      return 0
+    when 4, 5, 6
+      return 33
+    when 7, 8, 9
+      return 66
+    end
+  end
+
+  def quantity(fleet)
+  	fleet.quantity if fleet.groupable
+  end
+
+  def style(fleet)
+  	style = []
+  	style << "color: #{ fleet.squad.color }; font-weight: bold; font-size: 70%; width: 3%;"
+  	style.join(' ')
+  end
+end
