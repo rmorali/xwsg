8c50f8fb3848d5e962e65ff454818ecb5b4da29b
rmorali <rodrigo.morali@gmail.com>
2019-06-04 15:04:38 -0700

    implementing fog

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -13,5 +13,10 @@ class Planet < ApplicationRecord
   def under_attack?
     return true if fleets.distinct.count('squad_id') > 1
   end
+
+  def fog_for(squad)
+    results.where.not(squad: squad)
+    # TODO: Show fog of planet based on results table
+  end
   # TODO: Verify if a squad can build facilities, produce units etc
 end
