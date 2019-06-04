595adce4c17598cc4fbf020831ba89b0d75512e8
rmorali <rodrigo.morali@gmail.com>
2019-06-04 09:34:55 -0700

    moving

--- spec/services/move_fleet_spec.rb
+++ spec/services/move_fleet_spec.rb
@@ -4,30 +4,32 @@ RSpec.describe MoveFleet, type: :service do
   let(:fleet) { create(:fleet) }
   let(:unit) { create(:unit) }
   let(:squad) { create(:squad) }
+  let(:planet) { create(:planet) }
 
   context 'movement' do
-    before do
-      @origin = create(:planet)
+    before(:each) do
+      @origin = planet
       @destination = create(:planet)
       Route.create(vector_a: @origin, vector_b: @destination, distance: 2)
-      @fleet = create(:fleet, quantity: 10, unit: unit, squad: squad, planet: @origin, round: Round.current)
+      @fleet = fleet
+      @fleet.update(quantity: 10, unit: unit, squad: squad, planet: @origin, round: Round.current)
+      MoveFleet.new(@fleet, 10, @destination).order!
     end
     it 'orders a fleet to move' do
-      MoveFleet.new(@fleet, 10, @destination).order!
       expect(@fleet.destination).to eq(@destination)
     end
     it 'calculates the round of arrival' do
-      MoveFleet.new(@fleet, 10, @destination).order!
       expect(@fleet.arrives_in).to eq(2)
     end
     it 'takes its cargo with it' do
-      fleet.carrier = @fleet
-      fleet.save
+      fleet.update(carrier: @fleet)
       expect(@fleet.cargo).to include(fleet)
       MoveFleet.new(@fleet, 10, @destination).order!
       expect(fleet.reload.destination).to eq(@destination)
     end
     it 'splits a fleet in a partial movement' do
+      @fleet = create(:fleet)
+      @fleet.update(quantity: 10, unit: unit, squad: squad, planet: @origin, round: Round.current)
       MoveFleet.new(@fleet, 6, @destination).order!
       expect(Fleet.count).to_not eq(1)
       expect(@fleet.quantity).to eq(6)
@@ -36,15 +38,13 @@ RSpec.describe MoveFleet, type: :service do
       expect(Fleet.last.destination).to_not eq(@destination)
     end
     it 'unloads cargo in a partial movement' do
-      fleet.carrier = @fleet
-      fleet.save
+      fleet.update(carrier: @fleet)
       expect(@fleet.cargo).to include(fleet)
       MoveFleet.new(@fleet, 6, @destination).order!
       expect(@fleet.cargo).to_not include(fleet)
     end
     it 'cancel movement orders but doesnt unload carried fleets' do
-      fleet.carrier = @fleet
-      fleet.save
+      fleet.update(carrier: @fleet)
       MoveFleet.new(@fleet, 10, @destination).order!
       expect(@fleet.destination).to eq(@destination)
       expect(fleet.reload.destination).to eq(@destination)
