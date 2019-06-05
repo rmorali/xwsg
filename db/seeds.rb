0866ef84f5335570d52fa9f690adc363560d7094
rmorali <rodrigo.morali@gmail.com>
2019-06-04 18:08:16 -0700

    Better seeds for tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,8 @@ Faction.create([
 
 Squad.create([
                { name: 'Empire', credits: 1000, metals: 2000, rare_elements: 50, color: 'green', faction: Faction.first },
-               { name: 'Rebel', credits: 1000, metals: 2000, rare_elements: 50, color: 'red', faction: Faction.second }
+               { name: 'Rebel', credits: 1000, metals: 2000, rare_elements: 50, color: 'red', faction: Faction.second },
+               { name: 'Mercenary', credits: 1000, metals: 2000, rare_elements: 50, color: 'yellow', faction: Faction.third }
              ])
 
 emp = ['Empire']
@@ -36,17 +37,13 @@ Unit.create(name: 'Luke Skywalker', type: 'Diplomat', terrain: 'Special',
 Unit.create(name: 'Barrack', type: 'Facility', terrain: 'Ground',
             hyperdrive: 0, producing_time: 2, weight: 50, capacity: 50, groupable: false, carriable: false).factions = all
 
-anaxes = Planet.create(name: 'Anaxes', sector: 5, population: 1_502_500_345, x: 26, y: 4)
-byss = Planet.create(name: 'Byss', sector: 5, population: 1_522_900_890, x: 5, y: 38)
-corellia = Planet.create(name: 'Corellia', sector: 5, population: 2_102_270_234, x: 60, y: 72)
-coruscant = Planet.create(name: 'Coruscant', sector: 5, population: 999_302_444_400, x: 43, y: 38)
-fresia = Planet.create(name: 'Fresia', sector: 5, population: 7_902_070_999, x: 60, y: 4)
-kuat = Planet.create(name: 'Kuat', sector: 5, population: 9_502_600_900, x: 77, y: 38)
-mandalore = Planet.create(name: 'Mandalore', sector: 5, population: 202_055_077, x: 26, y: 72)
-
-hoth = Planet.create(name: 'Hoth', sector: 4, population: 50_200_100, x: 43, y: 38)
-tatooine = Planet.create(name: 'Tatooine', sector: 8, population: 15_900_500, x: 43, y: 38)
-mon_calamari = Planet.create(name: 'Mon Calamari', sector: 6, population: 302_444_400, x: 43, y: 38)
+anaxes = Planet.create(name: 'Anaxes', sector: 1, population: 1_502_500_345, x: 26, y: 4)
+byss = Planet.create(name: 'Byss', sector: 1, population: 1_522_900_890, x: 5, y: 38)
+corellia = Planet.create(name: 'Corellia', sector: 1, population: 2_102_270_234, x: 60, y: 72)
+coruscant = Planet.create(name: 'Coruscant', sector: 1, population: 999_302_444_400, x: 43, y: 38)
+fresia = Planet.create(name: 'Fresia', sector: 1, population: 7_902_070_999, x: 60, y: 4)
+kuat = Planet.create(name: 'Kuat', sector: 1, population: 9_502_600_900, x: 77, y: 38)
+mandalore = Planet.create(name: 'Mandalore', sector: 1, population: 202_055_077, x: 26, y: 72)
 
 Route.create([
                { vector_a: anaxes, vector_b: fresia, distance: 1 },
@@ -60,10 +57,7 @@ Route.create([
                { vector_a: coruscant, vector_b: corellia, distance: 1 },
                { vector_a: coruscant, vector_b: kuat, distance: 1 },
                { vector_a: kuat, vector_b: corellia, distance: 1 },
-               { vector_a: mandalore, vector_b: corellia, distance: 1 },
-               { vector_a: kuat, vector_b: mon_calamari, distance: 1 },
-               { vector_a: byss, vector_b: hoth, distance: 1 },
-               { vector_a: corellia, vector_b: tatooine, distance: 1 }
+               { vector_a: mandalore, vector_b: corellia, distance: 1 }
              ])
 
 round = Round.current
@@ -73,7 +67,10 @@ Fleet.create(unit: Unit.second, quantity: 1, squad: Squad.first, planet: corusca
 Fleet.create(unit: Unit.find(4), quantity: 10, squad: Squad.first, planet: coruscant, round: round)
 Fleet.create(unit: Unit.find(5), quantity: 10, squad: Squad.first, planet: coruscant, round: round)
 
-Fleet.create(unit: Unit.first, quantity: 1, squad: Squad.second, planet: corellia, round: round)
-Fleet.create(unit: Unit.third, quantity: 1, squad: Squad.second, planet: corellia, round: round)
-Fleet.create(unit: Unit.find(4), quantity: 10, squad: Squad.second, planet: corellia, round: round)
-Fleet.create(unit: Unit.find(5), quantity: 10, squad: Squad.second, planet: corellia, round: round)
+Fleet.create(unit: Unit.first, quantity: 1, squad: Squad.second, planet: coruscant, round: round)
+Fleet.create(unit: Unit.third, quantity: 1, squad: Squad.second, planet: coruscant, round: round)
+Fleet.create(unit: Unit.find(4), quantity: 10, squad: Squad.second, planet: coruscant, round: round)
+Fleet.create(unit: Unit.find(5), quantity: 10, squad: Squad.second, planet: coruscant, round: round)
+
+Fleet.create(unit: Unit.first, quantity: 1, squad: Squad.third, planet: mandalore, round: round)
+
