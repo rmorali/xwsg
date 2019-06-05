2f5204d1487f1a3ae28644541b93ad53edfd3591
rmorali <rodrigo.morali@gmail.com>
2019-06-05 10:15:29 -0700

    set income

--- app/services/set_income.rb
+++ app/services/set_income.rb
@@ -1,5 +1,5 @@
 class SetIncome
-  def initialize(planet, squad)
+  def initialize(squad, planet)
     @squad = squad
     @planet = planet
   end
@@ -15,11 +15,20 @@ class SetIncome
     @planet.fleets_presence.each do |f|
       squad = f[0]
       squad_presence = f[1]
+      squad_presence = 20000 if squad_presence > 20000
       if squad.id == @squad.id
         squad_ratio = squad_presence.to_f / presence_for_domination.to_f
         income = @planet.credits.to_f * squad_ratio.to_f * ratio.to_f
       end
     end
     income.to_i
+  end
+
+  def total
+    income = 0
+    Planet.seen_by(@squad).each do |p|
+      income += SetIncome.new(@squad, p).current
+    end
+    income
   end
 end
