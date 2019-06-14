dd9f0c081d32ec276cf56234b90eebf398513861
rmorali <rodrigo.morali@gmail.com>
2019-06-14 07:29:32 -0700

    eager load

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,10 +8,10 @@ Rails.application.configure do
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = false
+  config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
