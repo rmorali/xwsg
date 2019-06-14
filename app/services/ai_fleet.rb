ff18cd6e768a88f36c795a4e3a588c3679ba34aa
rmorali <rodrigo.morali@gmail.com>
2019-06-14 05:37:27 -0700

    building AI facilities

--- app/services/ai_fleet.rb
+++ app/services/ai_fleet.rb
@@ -2,23 +2,61 @@ class AiFleet
   def initialize(*squad)
     @squad = squad.first unless squad.nil?
     @setup = Setup.current
+    @round = Round.current
   end
 
   def act!
     return unless @squad.ai == true
     fleets = Fleet.where(ai: true, squad: @squad)
+
+    if @squad.credits > 1200
+      chance_of_build = rand(@squad.ai_level..6)
+      build! if chance_of_build == 6
+    end
+
+    facilities = fleets.select { |fleet| fleet.type == 'Facility'}
+    credits_for_producing = @squad.credits
+    credits_for_producing = @squad.credits / facilities.count unless facilities.empty?
+    facilities.each do |facility|
+      produce!(facility, credits_for_producing) unless @round.number == 1
+    end
+
     carriers = fleets.select { |fleet| fleet.available_capacity > 10 && fleet.type != 'Facility' }
     carriers.each do |carrier|
       embark!(carrier)
-    end 
+    end
+    
     fleets.each do |fleet|
       move!(fleet)
     end
     @squad.ready!
   end
 
-  def produce
-
+  def produce!(facility, available)
+    for_capital_ships = available * 0.40
+    for_transports = available * 0.10
+    for_fighters = available * 0.50
+    planet = facility.planet
+    squad = facility.squad
+    until for_capital_ships < 300 do
+      capital_ships = Unit.allowed_for(squad.faction.name).where("type = ? AND credits <= ?", 'CapitalShip', for_capital_ships)
+      capital_ship = capital_ships[rand(capital_ships.count)] unless capital_ships.empty?
+      BuildFleet.new(1, capital_ship, squad, planet).build! unless capital_ship.nil?
+      for_capital_ships -= capital_ship.credits
+    end
+    until for_transports < 100 do
+      transports = Unit.allowed_for(squad.faction.name).where("type = ? AND credits <= ?", 'HeavyTransport', for_transports)
+      transport = transports[rand(transports.count)] unless transports.empty?
+      BuildFleet.new(1, transport, squad, planet).build! unless transport.nil?
+      for_transports -= transport.credits
+    end
+    for_fighters = for_fighters / 2
+    2.times do
+      fighters = Unit.allowed_for(squad.faction.name).where("type = ? AND credits <= ?", 'Fighter', for_fighters)
+      fighter = fighters[rand(fighters.count)] unless fighters.empty?
+      BuildFleet.new( (for_fighters / fighter.credits).to_i, fighter, squad, planet).build! unless fighter.nil?
+    end
+    GroupFleet.new(planet).group!
   end
 
   def embark!(fleet)
@@ -40,8 +78,12 @@ class AiFleet
     end
   end
 
-  def build
-
+  def build!
+    planets = Planet.seen_by(@squad).reject { |p| p.fleets.any? { |f| f.type == 'Facility' } }
+    planet = planets[rand(planets.count)] unless planets.empty?
+    facilities = Unit.allowed_for(@squad.faction.name).where("type = ? AND credits <= ?", 'Facility', @squad.credits)
+    facility = facilities[rand(facilities.count)] unless facilities.empty?
+    BuildFleet.new(1, facility, @squad, planet).build! unless facility.nil? || planet.nil?
   end
 
   def choose_destination(routes)
