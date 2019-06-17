d35b38df3cc338ba8e9f14299c03446738f4fd93
rmorali <rodrigo.morali@gmail.com>
2019-06-17 09:02:22 -0700

    views

--- app/controllers/squads_controller.rb
+++ app/controllers/squads_controller.rb
@@ -32,8 +32,8 @@ class SquadsController < ApplicationController
 
   def map
     @map_ratio = 1
-    @map_x_adjust = 80
-    @map_y_adjust = 20
+    @map_x_adjust = 10
+    @map_y_adjust = 70
     @squad = current_squad
     @squad_credits = @squad.credits
     @squad_income = SetIncome.new(@squad, Planet.first).total
