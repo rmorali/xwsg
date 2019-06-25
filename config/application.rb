2b7fbaf63f1ebd79781311bca0b0650d8a98eb6a
rmorali <rodrigo.morali@gmail.com>
2019-06-25 09:42:47 -0700

    disabled sendgrid for now

--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-require 'sendgrid-ruby'
-
-include SendGrid
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
