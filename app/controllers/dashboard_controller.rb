9bef01eb3b3dc07716586a122ec77245c387eca7
rmorali <rodrigo.morali@gmail.com>
2020-04-27 15:29:47 -0300

    before action in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,19 @@
 class DashboardController < ApplicationController
-
+  before_action :load_objects, except: [:teste]
   def index
-
+    
   end
 
   def show
-    @squads = Squad.all
-    @planets = Planet.all
+
   end
 
+  def teste
+  end
+
+  private
+    def load_objects
+      @squads = Squad.all
+      @planets = Planet.all
+    end
 end
--- app/views/dashboard/show.json.jbuilder
+++ app/views/dashboard/show.json.jbuilder
@@ -1,1 +1,2 @@
 json.squads @squads
+json.planets @planets
