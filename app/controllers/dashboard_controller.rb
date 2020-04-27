b647c190bb4896530a6eaab2156f925296eafb61
rmorali <rodrigo.morali@gmail.com>
2020-04-27 14:10:27 -0300

    dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,11 +6,7 @@ class DashboardController < ApplicationController
 
   def show
     @squads = Squad.all
-    @squad = current_squad
-    @squad_income = SetIncome.new(@squad, Planet.first).total
     @planets = Planet.all
-    @round = Round.current
-    @setup = Setup.current
   end
 
 end
