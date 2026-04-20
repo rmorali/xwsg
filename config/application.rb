2ba175e315b90ce2bf7478862e3e8f2f24e1ec57
rmorali <rodrigo.morali@gmail.com>
2026-04-20 09:51:05 -0300

    upgrade to rails 7.2

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Xwsg
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1 # ATUALIZADO: Configurações padrão para Rails 7.0
+    config.load_defaults 7.2 # ATUALIZADO: Configurações padrão para Rails 7.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
